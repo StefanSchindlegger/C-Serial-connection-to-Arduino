683a885fc429a2f8c713a622bb47486903a18663
StefanSchindlegger <s.schindlegger@aon.at>
2017-05-14 10:35:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+
+namespace SerialConnectionToArduino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string portName = "com6";
+            int boudRate = 9600;
+            bool connected = false;
+            
+            SerialPort sP = new SerialPort();
+            sP.PortName = portName;
+            sP.BaudRate = boudRate;
+            sP.ReadTimeout = 500;
+            sP.WriteTimeout = 500;
+            try
+            {   sP.Open();
+                connected = true;
+            }catch (Exception ex){
+                Console.WriteLine("Could not connect!");
+            }
+          
+           // sP.WriteLine("Hello");
+
+            while (connected)
+            {
+                Console.WriteLine(System.DateTime.Now.ToString()+ ": "+ sP.ReadLine());
+               
+            }
+
+        }
+    }
+}
---
+++ SerialToCsharp.ino
@@ -0,0 +1,17 @@
+int potentiometerValue = 0;
+int toSend = 0;
+int potentiometer = A0;
+
+void setup(){  
+  pinMode(potentiometer, INPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  potentiometerValue = analogRead(potentiometer);
+  toSend = map(potentiometerValue, 0, 1023, 0, 255);  
+  Serial.println("Ich bin ein Text :)");
+  Serial.println(toSend);
+  
+  delay(300);
+}
